f10a203db29b5b7fb21fef9fabe1a8e28cfdc87d
Elio <eliokazu@gmail.com>
2017-07-18 23:48:36 -0400

    Price is now showing at the top

--- App.js
+++ App.js
@@ -1,35 +1,62 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import { AppRegistry, Text, View, StyleSheet } from 'react-native';
 
-var CurrentPrice = React.createClass({
 
-  render() {
+const styles = StyleSheet.create({
+  loading: {},
+  priceView: {},
+  mainView: {
+    flex: 1,
+    backgroundColor: 'skyblue'
+  },
+  center: {
+    alignItems: 'center'
+  }
+});
+
+class CurrentPrice extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoading: true
+    }
+  }
+
+  componentWillMount() {
     let self = this
-
-    self.state = {}
-    self.state.loading = true
-
     let book = 'eth_cad'
     fetch('https://api.quadrigacx.com/v2/ticker?book=' + book)
-        .then((res) => res.json())
-        .then((json) => {
-          console.log(json)
-          self.state.isLoading = false
-          self.state.data = json
+      .then((res) => res.json())
+      .then((json) => {
+        self.setState({
+          isLoading: false,
+          data: json
         })
+      })
+      .catch(err => {
+        console.error(err)
+        throw err
+      }) 
+  }
 
-    if (this.state.loading) {
+  render() {
+
+    if (this.state.isLoading) {
       return (<Text>Loading...</Text>); 
     }
-    console.log(this.state.data)
-    return (<Text>{this.state.data}</Text>)
+    return (
+      <View style={styles.priceView, styles.center}>
+        <Text>{this.state.data.last}</Text>
+      </View>
+    )
   }
-})
+}
 
 export default class Main extends Component {
   render() {
     return (
-      <View style={{alignItems: 'center'}}>
+      <View style={styles.mainView}>
         <CurrentPrice />
       </View>
     );
